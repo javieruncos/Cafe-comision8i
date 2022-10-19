35c50309320a51ab0e9cfd6c45c9024d5ba41613
Javier Uncos <javiersrfc30@gmail.com>
2022-10-19 18:40:02 -0300

    agregos las midlewares y las rutas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
-
+import cors from "cors"
+import morgan from "morgan"
 
 // crear una instancia de express 
 const app = express();
@@ -7,9 +8,18 @@ const app = express();
 app.set("port",process.env.PORT ||  4000)
 
 app.listen(app.get("port"), ()=>{
-    console.log("estoy en el puerto"+ app.get("port"))
+    console.log("estoy en el puerto "+ app.get("port"))
 })
 
 
+// midlewares: funsiones que se ejecutan antes de las rutas
 
-console.log("hola mundo")
+app.use(cors()); // permite conexiones remotas
+app.use(express.json()); // permiten interpretar el formato json
+app.use(express.urlencoded({extends:true}));
+
+
+// rutas 
+
+
+
