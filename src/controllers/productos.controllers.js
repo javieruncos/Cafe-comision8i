46069afa17c023a3282e0a8f5bee8cd3dafa8b07
Javier Uncos <javiersrfc30@gmail.com>
2022-10-26 17:01:11 -0300

    agregamos la logica para listar productos y devolver un arreglo

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -3,8 +3,18 @@ import Producto from "../models/producto"
 
 
 
-export  const listarProductos = (req,res)=>{
-    res.send("esto es una prueba de la peticion get")
+export  const listarProductos = async(req,res)=>{
+    try{
+    //  buscar en la base de datos la collection de productos
+    const productos = await Producto.find();
+    // envio la respuesta
+    res.status(200).json({productos})
+    }catch(error){
+        console.log(error)
+        res.status(404).json({
+            mensaje:"error al buscar productos"
+        })
+    }
 }
 
 export  const crearProductos = async(req,res)=>{
