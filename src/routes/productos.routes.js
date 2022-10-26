1bd1513f9cf5f0b0679cbebf43e296b4beb43cc7
Javier Uncos <javiersrfc30@gmail.com>
2022-10-26 18:38:07 -0300

    agregamos las validaciones con express validator

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,14 +1,45 @@
 import { Router } from "express";
-import { borrarProducto, crearProductos, editarProducto, listarProductos, obtenerProductos } from "../controllers/productos.controllers";
-
+import {
+  borrarProducto,
+  crearProductos,
+  editarProducto,
+  listarProductos,
+  obtenerProductos,
+} from "../controllers/productos.controllers";
+import { check } from "express-validator";
 const router = Router();
 
-router.route('/productos').get(listarProductos).post(crearProductos);
-router.route("/productos/:id")
-.get(obtenerProductos)
-.put(editarProducto)
-.delete(borrarProducto)
-
+router
+  .route("/productos")
+  .get(listarProductos)
+  .post(
+    [
+      check("nombreProducto")
+        .notEmpty()
+        .withMessage("El nombre del producto es obligatorio")
+        .isLength({min:2,max:100})
+        .withMessage("El nombre del producto debe tener 2 y 100 caracteres"),
+      check("precio")
+      .notEmpty()
+      .withMessage("el precio es obligatorio")
+      .isNumeric()
+      .withMessage("el precio debe ser un numero")
+      .custom((value)=>{
+         if(value >= 1 && value <= 10000){
+            return true
+         }else{
+            throw new Error("el precio debe estar entre 1 y 10000")
+         }
+      })
+      ,
+    ],
+    crearProductos
+  );
+router
+  .route("/productos/:id")
+  .get(obtenerProductos)
+  .put(editarProducto)
+  .delete(borrarProducto);
 
 // app.get("/prueba",(req,res)=>{
 //     res.send("esto es una prueba de una peticioon get")
