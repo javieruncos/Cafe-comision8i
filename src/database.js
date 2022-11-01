91ca8491e201cfd28ca4cce0bdd1716f1df7fadc
Javier Uncos <javiersrfc30@gmail.com>
2022-11-01 18:52:27 -0300

    nos conectamos a la base de datos d produccion

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const url = "mongodb://localhost:27017/cafe-c8i";
+// const url = "mongodb://localhost:27017/cafe-c8i"; // base de datos local
+const url = "mongodb+srv://JavierSrfc:Riversrfc1010@cluster0.3kg4e1g.mongodb.net/test";
 // const url = "mongodb://127.0.0.1:27017/cafe-c8i";
 mongoose.connect(url);
 
